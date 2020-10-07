f34be02e72b939841974031adc227e8cf793a809
dawiddd95 <dawlyc@wp.pl>
2020-10-07 11:55:35 +0200

    Created route for candidate list url

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .serializers import CandidateSerializer
+from .models import Candidate
+
+def candidate_list(request):
+   candidates = Candidate.objects.all()
+   serializer = CandidateSerializer(candidates, many=True)
+ 
+   return JsonResponse({'data': serializer.data})
